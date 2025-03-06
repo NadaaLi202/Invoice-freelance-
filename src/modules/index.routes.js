22c36a35e86548824abb3e838c216685497c60f1
Nada Ali <nadaaliwazeer@gmail.com>
2025-03-06 16:09:14 +0200

    fix vercel deployment

--- src/modules/index.routes.js
+++ src/modules/index.routes.js
@@ -14,6 +14,7 @@ app.use('/api/v1/users',userRouter)
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/invoices',invoiceRouter)
 app.use('/api/v1/products',productRouter)
+console.log('hello')
 
     app.use('*', (req,res,next) => {
         // res.status(404).json({message : 'Route not found'})
